9fb645ec073c7ea93633d2529141a717ef93a8fd
Samarth Gambhir <71920758+gambhirsamarth@users.noreply.github.com>
2023-05-25 22:44:28 +0530

    Create Day21.js

---
+++ Day21.js
@@ -0,0 +1,10 @@
+var chunk = function(arr, size) {
+  var ans = [];
+  var index = 0;
+
+  while (index < arr.length) {
+    ans.push(arr.slice(index, index + size));
+    index += size;
+  }
+  return ans;
+}
