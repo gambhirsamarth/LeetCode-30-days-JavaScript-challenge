4598336ad88665818ba78e174bb1d1bde0e0755d
Samarth Gambhir <71920758+gambhirsamarth@users.noreply.github.com>
2023-06-03 20:07:05 +0530

    Create Day30.js

---
+++ Day30.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+var inorderTraversal = function*(arr) {
+    for(let i=0; i<arr.length;i++){
+        if(Array.isArray(arr[i])){
+            yield* inorderTraversal(arr[i])
+        } else {
+            yield arr[i];
+        }
+    }
+};
